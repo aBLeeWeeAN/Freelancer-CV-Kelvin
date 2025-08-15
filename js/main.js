6a054c53877300e3e556614df3b8e8f240688980
Vladimir Nikulin <mail.jorey@gmail.com>
2025-08-15 19:29:37 +0300

    Добавлена мобильная навигация

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
-var a = 4;
-var b = 3;
+const nav = document.getElementById("nav");
+const navBtn = document.getElementById("nav-btn");
+const navBtnImg = document.getElementById("nav-btn-img");
+
+navBtn.onclick = () => {
+    if (nav.classList.toggle("open")) {
+        navBtnImg.src = "./img/icons/nav_close.svg";
+    } else {
+        navBtnImg.src = "./img/icons/nav_open.svg";
+    }
+};
